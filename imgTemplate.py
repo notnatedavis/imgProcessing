f9a14f82a1fdb7f14c67fc990c64e763f94c343d
Nathaniel Davis-Perez <ndavispe@asu.edu>
2025-06-13 11:20:20 -0400

    new imgTemplate file

---
+++ imgTemplate.py
@@ -0,0 +1,14 @@
+# --- imgTemplate.py --- #
+# intent here
+
+# notes : 
+
+# --- Imports --- #
+
+# Hardcoded variables
+
+# --- Helper Functions --- #
+
+# --- Main Entry Point --- #
+if __name__ == "__main__" :
+    pass
